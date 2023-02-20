f00861aa5fd19a4b1e056da958207bb70798847b
Murshid Alam <65403832+murschid@users.noreply.github.com>
2023-02-20 13:16:26 +0000

    Imported classes

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 console.log('Welcome in Datastructire & Algorithm by JavaScript');
-import sort from './algorithm/Sorting.js';
+import sorting from './algorithm/Sorting.js';
+import searching from './algorithm/Searching.js';
+import general from './algorithm/General.js';
 
-console.log(sort.bubbleSort([1,3,6,2,4]));
+console.log(sorting.bubbleSort([1,3,6,2,4]));
